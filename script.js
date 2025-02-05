be4bf776600b3877245a294517399ac1637a8c85
abhaykumar-0285 <abhay.kr2803@gmail.com>
2025-02-05 17:22:47 +0530

    updating the website

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        alert("Thank you for contacting us!");
+        form.reset();
+    });
+});
