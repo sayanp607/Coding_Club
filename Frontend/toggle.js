5e48d86364a3ad2d02b8ff89a91743993717873e
sarbik <sarbikchatterjee99@gmail.com>
2025-02-15 04:13:05 +0530

    fixed responsiveness in the contributor page

--- Frontend/toggle.js
+++ Frontend/toggle.js
@@ -1,3 +1,4 @@
 document.getElementById("menu-toggle").addEventListener("click", function() {
   document.getElementById("navbar").classList.toggle("active");
+  document.querySelector(".team-container").classList.toggle("shifted");
 });
